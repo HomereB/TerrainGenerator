eb256911ab8fd18836259615e3d2c537724b563b
TheWincher <louchez26@hotmail.fr>
2019-12-02 12:35:29 +0100

    1ere version de gen de terrain

---
+++ TerrainGenerator/Assets/Scripts/TerrainGenerator.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TerrainGenerator : MonoBehaviour
+{
+
+    public int width, height, depth, scale;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Terrain terrain = GetComponent<Terrain>();
+        terrain.terrainData = GenerateTerrain(terrain.terrainData);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    TerrainData GenerateTerrain(TerrainData terrainData)
+    {
+        terrainData.heightmapResolution = width + 1;
+        terrainData.size = new Vector3(width, depth, height);
+        terrainData.SetHeights(0, 0, GenerateHeights());
+
+        return terrainData;
+    }
+
+    float[,] GenerateHeights()
+    {
+        float[,] heights = new float[width, height];
+        for(int x = 0; x < width; x++)
+        {
+            for(int y = 0; y < height; y++)
+            {
+                heights[x, y] = CalculateHeight(x, y);
+            }
+        }
+
+        return heights;
+    }
+
+    float CalculateHeight(int x,  int y)
+    {
+        float xCoord = (float) x / width * scale;
+        float yCoord = (float) y / height * scale;
+
+        return Mathf.PerlinNoise(xCoord, yCoord);
+    }
+}
