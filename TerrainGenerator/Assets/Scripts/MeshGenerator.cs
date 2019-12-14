30480c5fb5971a1f9f9638f775a3664dbe416305
Homère <homerebourgeois@hotmail.fr>
2019-12-14 22:48:13 +0100

    obtention graphe d'écoulement correct

--- TerrainGenerator/Assets/Scripts/MeshGenerator.cs
+++ TerrainGenerator/Assets/Scripts/MeshGenerator.cs
@@ -5,19 +5,19 @@ using UnityEngine;
 
 public class MeshGenerator : MonoBehaviour
 {
-    // Start is called before the first frame update
-    Mesh mesh;
-    Vector3[] vertices;
-    int[] triangles;
+    [Space(15)]
 
+    [Header("Generation du terrain")]
 
-
+    [Space(10)]
     public int seed;
-
+    [Space(10)]
     public int xSize =256;
     public int zSize = 256;
     public int maxHeight;
     public int minHeight;
+
+    [Space(10)]
 
     public float mainOffsetX;
     public float mainOffsetZ;
@@ -25,19 +25,32 @@ public class MeshGenerator : MonoBehaviour
     public float secondaryOffsetX;
     public float secondaryOffsetZ;
 
+    [Space(10)]
     public float mainScale = 1;
     public float secondaryScale = 1;
 
     public float mainNoiseMultiplicator;
     public float secondaryNoiseMultiplicator;
 
+    [Space(10)]
+    public TerrainType[] regions;
+
+    [Space(15)]
+    [Header("Generation des rivières")]
+
+    [Space(10)]
+    public int nbSources;
+    public List<Vector2Int> sourceCoordinates;
+    public float heightTolerance;
+    public List<List<int>> flowGraphs;
+
+    Mesh mesh;
+    Vector3[] vertices;
+    int[] triangles;
     Renderer renderr;
     Texture2D texture;
 
     System.Random rng;
-
-    public TerrainType[] regions;
-
 
     void Start()
     {
@@ -53,19 +66,81 @@ public class MeshGenerator : MonoBehaviour
 
         renderr.material.mainTexture = GenerateTexture();
         CreateShape();
+
+        sourceCoordinates.Add(new Vector2Int(24, 24));
+        //renderr.sharedMaterial.mainTexture = GenerateColorTexture();
+        flowGraphs = new List<List<int>>(sourceCoordinates.Count);
+        for(int i = 0; i<sourceCoordinates.Count;i++)
+        {
+            flowGraphs.Add(new List<int>());
+            GenerateFlowingGraph(i);
+            vertices[(xSize+1)* sourceCoordinates[0].y+ sourceCoordinates[0].x].y -=5;
+        }
         UpdateMesh();
-        renderr.material.mainTexture = GenerateColorTexture();      
     }
 
-     private Texture2D GenerateColorTexture()
+    private void GenerateFlowingGraph(int sourceIndex)
+    {
+        List<Vector2Int> riverPoints = new List<Vector2Int>
+        {
+            new Vector2Int(sourceCoordinates[sourceIndex].x, sourceCoordinates[sourceIndex].y)
+        };
+        List<int> exploredIndexes = new List<int>
+        {
+            sourceCoordinates[sourceIndex].x+ sourceCoordinates[sourceIndex].y*(xSize+1)
+        };
+        while(riverPoints.Count!=0)
+        {
+            float currentHeight = vertices[riverPoints[0].x+ riverPoints[0].y*(xSize+1)].y;
+            int nextIndexX = -1;
+            int nextIndexZ = -1;
+            Debug.Log(riverPoints[0].x + riverPoints[0].y * (xSize + 1));
+            for(int i = -1 ; i <=  1 ; i++)
+            {
+                for (int j = - 1 ; j <= 1 ; j++)
+                {
+                    int index = riverPoints[0].x + i + ((riverPoints[0].y + j) * (xSize + 1));
+                    
+                    if(i != 0 && j != 0 && riverPoints[0].x + i + ((riverPoints[0].y + j) * (xSize + 1))>=0 && riverPoints[0].x + i + ((riverPoints[0].y + j) * (xSize + 1))< (xSize+1)*(zSize+1) )
+                    {
+                        if(currentHeight + heightTolerance > vertices[riverPoints[0].x + i + ((riverPoints[0].y + j) * (xSize + 1))].y && !exploredIndexes.Contains(riverPoints[0].x + i + ((riverPoints[0].y + j) * (xSize + 1))))
+                        {                      
+                            currentHeight = vertices[index].y;
+                            nextIndexX = riverPoints[0].x + i ;
+                            nextIndexZ = (riverPoints[0].y + j);
+                        }
+
+                    }
+
+                }
+            }
+            if (nextIndexZ!=-1)
+            {
+                riverPoints.RemoveAt(0);
+                riverPoints.Add(new Vector2Int(nextIndexX,nextIndexZ));
+                flowGraphs[0].Add(nextIndexX+nextIndexZ * (xSize + 1));
+                exploredIndexes.Add(nextIndexX+ nextIndexZ * (xSize + 1));
+            }
+            else
+            {
+                break;
+            }
+        }
+        foreach(int i in flowGraphs[0])
+        {
+            vertices[i].y -= 2.5f;
+        }
+    }
+
+    private Texture2D GenerateColorTexture()
      {
-        Color[] colourMap = new Color[xSize * zSize];
+        Color[] colourMap = new Color[(xSize+1) * (zSize+1)];
 
         Texture2D texture = new Texture2D(xSize, zSize);
         texture.filterMode = FilterMode.Point;
-        for (int i = 0, z = 0; z < zSize; z++)
+        for (int i = 0, z = 0; z <= zSize; z++)
         {
-            for (int x = 0; x < xSize; x++)
+            for (int x = 0; x <= xSize; x++)
             {
                 float currentHeight = vertices[i].y;
                 for(int j=0 ;j<regions.Length;j++)
@@ -96,7 +171,6 @@ public class MeshGenerator : MonoBehaviour
                 texture.SetPixel(x, z, color);
             }
         }
-        texture.Apply();
         return texture;
     }
 
